7cd312a4cb3759754c8b83d05950e697053b5c60
dcojan <myglorybox@hotmail.com>
2014-04-10 13:46:14 +0200

    starting visual

---
+++ includes/table.php
@@ -0,0 +1,15 @@
+<h1>WARHAMMER</h1>
+
+<table id="board">
+<?php
+for ($i=0;$i<150;$i++)
+{
+	echo "<tr>";
+	for ($j=0;$j<100;$j++)
+	{
+		echo "<td>" . "</td>";
+	}
+	echo "</tr>";
+}
+?>
+</table>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE>
+<html>
+<head>
+<link rel="stylesheet" href="styles/table.css" type="text/css">
+<link rel="stylesheet" href="styles/index.css" type="text/css">
+</head>
+<body>
+
+ <?php
+	 require_once('includes/table.php');
+?>
+</body>
+</html>
